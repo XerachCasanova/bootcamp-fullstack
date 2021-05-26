1bfb7d07af7259aa713e6f7cd6f011785189716d
Xerach Casanova <xermam1984@hotmail.com>
2021-05-26 19:10:15 +0100

    part4 - Exercise 4.1 - 4.2 v1.0

--- pruebas/part4/backend/models/note.js
+++ pruebas/part4/backend/models/note.js
@@ -1,5 +1,23 @@
 const mongoose = require('mongoose')
 
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }
+).then(result => {
+  console.log('Connected to MongoDB')
+}).catch(error => {
+
+  console.log('Error connecting to MongoDB:', error.message)
+})
+
 const noteSchema = new mongoose.Schema(
   {
     content: {
