4af3f7a611948591928d881dee0cb38a3361fc8a
Xerach Casanova <xermam1984@hotmail.com>
2021-06-16 16:43:13 +0100

    part 5 done

---
+++ part5/bloglist-backend/app.js
@@ -0,0 +1,38 @@
+const config = require('./utils/config')
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+console.log(config.MONGODB_URI)
+logger.info('coneccting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(logger.info('Connected to MongoDB'))
+  .catch(error => logger.error('error connecting to MongoDB: ', error.message))
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
+
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
